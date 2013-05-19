21babfd3365078b316dc926e5c41ba539bad03a9
Rob Lanfear <rob.lanfear@gmail.com>
2013-05-20 09:04:54 +1000

    new initiation script

--- treemaker.py
+++ treemaker.py
@@ -5,4 +5,9 @@ from maker import main
 
 if __name__ == "__main__":
     # Well behaved unix programs exits with 0 on success...
-    sys.exit(main.main())
+    sppfile = "/Users/Rob/Documents/Projects_Current/treemaker/example/spplist.txt"
+    alnfile = "/Users/Rob/Documents/Projects_Current/treemaker/example/test.phy"
+    match_genera = False
+    
+    
+    sys.exit(main.main(sppfile, alnfile, match_genera))
