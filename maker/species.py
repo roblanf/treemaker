8b85f0ddafe1d61b72a8e3b8ab2d6bce38eb702e
Rob Lanfear <rob.lanfear@gmail.com>
2013-05-20 09:03:51 +1000

    species object improved

--- maker/species.py
+++ maker/species.py
@@ -1,59 +1,102 @@
 
 import genbank
 import tnrs
+import json
 
 import logging
-logging.basicConfig(level="INFO")
+log = logging.getLogger("species")
 
 class Species(object):
     """A Species file
     """
 
-    def __init__(self, name):
-        self.name = name
-        self.genus = self.name.split()[0]
-        self.species = self.name.split()[1]
-        self.binomial = " ".join([self.genus, self.species])
-        
-        # use the TNRS to get a name        
-        self.tnrs_response = tnrs.check_one_species(self.binomial)
-        if self.tnrs_response != "NA":
-            self.tnrs_acceptedname = tnrs.get_acceptedname(self.tnrs_response)
-            
-            if self.tnrs_acceptedname != self.name:
-                logging.warning("Species name '%s' is different from the TNRS name '%s'."
-                                " Please check for spelling mistakes." %(self.name, self.tnrs_acceptedname))
-            
+    def __init__(self, clean_name, original_name):
+        log.info("Starting new species: '%s'" % original_name)
+
+        self.clean_name = clean_name
+        self.original_name = original_name
+        self.original_binomial = self.make_binomial()
+        self.tnrs_acceptedname = None
+        self.tnrs_binomial = None
+        self.tnrs_genus = None
+        self.genbank_name = None
+        self.genbank_binomial = None
+        self.genbank_genus = None
+        self.data = None
+    
+    def __str__(self):
+        return self.original_name
+    
+    def make_binomial(self):
+        items = self.clean_name.split()
+        if len(items)>1:
+            binomial = ' '.join([items[0], items[1]])
         else:
-            self.tnrs_acceptedname = "NA"
+            binomial = ' '.join([items[0], "spp"])
+        return binomial
 
-        # use GenBank get a name
+    @property
+    def binomial(self):
+        if hasattr(self, '_binomial'):
+            return self._binomial
+        else:
+            checked_binomial = self.set_binomial(self.clean_name)
+            self._binomial = checked_binomial
+            return self._binomial
+    
+    def set_binomial(self, name):
+        log.debug("Checking spelling of '%s'" %(name))
+        genus = self.clean_name.split()[0]
+        species = self.clean_name.split()[1]
+        binomial = " ".join([genus, species])
         
-        # try a few things to get a taxonID:
-        self.taxid = genbank.get_taxid(self.name)
+        self.tnrs_acceptedname = tnrs.check_one_species(binomial)
+        tnrs_split = self.tnrs_acceptedname.split()
+        
+        if len(tnrs_split)>1:
+            self.tnrs_binomial = ' '.join([tnrs_split[0], tnrs_split[1]])
+        else:
+            self.tnrs_binomial = self.tnrs_acceptedname
 
-        if self.taxid == "NA":
-            logging.info("Couldn't find TaxonID for '%s'" % self.name)
-        
-            if self.binomial != self.name:
-                taxid2 = genbank.get_taxid(self.binomial)
-            else:
-                taxid2 = "NA"
-            
-            if self.tnrs_acceptedname != self.name and self.tnrs_acceptedname != "NA":
-                taxid3 = genbank.get_taxid(self.tnrs_acceptedname)
-            else:
-                taxid3 = "NA"
-        
-            if taxid2 == "NA" and taxid3 != "NA":  
-                logging.warning("Found TaxonID using TNRS name '%s', please check" %(self.tnrs_acceptedname))
-                self.taxid = taxid3
-            elif taxid3 == "NA" and taxid2 != "NA":  
-                logging.warning("Found TaxonID using binomial '%s', please check" %(self.binomial))
-                self.taxid = taxid2
-            else:
-                logging.info("Checked TNRS and binomial but still couldn't find TaxonID")
+        if binomial == self.tnrs_binomial:
+            checked_binomial = binomial
+            self.TNRSmatch = "perfect"
+            log.debug("Binomial matches TNRS")
+
+        elif binomial != self.tnrs_binomial and self.tnrs_binomial != "NA" and len(self.tnrs_binomial.split())>1:
+            log.warning("Spelling error detected. Binomial '%s' doesn't match TNRS "
+                            "'%s'" %(binomial, self.tnrs_binomial))
+            checked_binomial = self.tnrs_binomial
+            self.TNRSmatch = "imperfect"
+
+        else:
+            log.debug("Binomial '%s' not found on TNRS. Please check" %(name))
+
+            log.debug("Checking genus name only against TNRS...")
+            tnrs_genus = tnrs.check_one_species(genus)
+
+            if tnrs_genus == genus:
+                log.debug("Genus matches tnrs")
+                checked_binomial = ' '.join([tnrs_genus, species])
+                self.TNRSmatch = "genus"
                 
+            elif tnrs_genus != genus and tnrs_genus != "NA":
+                log.warning("Genus spelling error detected: input '%s' "
+                                "doesn't match TNRS '%s', please check" %(genus, tnrs_genus))
+                checked_binomial = ' '.join([tnrs_genus, species])
+                self.TNRSmatch = "genus_imperfect"
+            else:
+                log.warning("No genus match found for '%s', please check" % genus)
+                self.TNRSmatch = "none"
+                checked_binomial = binomial
+
+        log.info("Original binomial is    : '%s'" %(self.original_binomial))                
+        log.info("Spellchecked binomial is: '%s'" %(checked_binomial))
+        
+        return checked_binomial           
+        
+    def set_genbank_name(self, name):
+        self.set_taxonID(name)
         if self.taxid != "NA":
             self.lineage = genbank.get_lineage(self.taxid)
             self.genbank_binomial = " ".join([self.lineage['genus'], 
@@ -61,33 +104,41 @@ class Species(object):
         else:
             self.lineage = "NA"
             self.genbank_binomial = "NA"
+        
+        log.info("set genbank_binomial to: '%s'" %self.genbank_binomial)
+        
+             
+    def set_taxonID(self, name):
 
-        nameset = set([self.name, self.binomial, self.genbank_binomial, self.tnrs_acceptedname])
+        taxids = {}
+        nameset = set([self.name, self.binomial, self.tnrs_acceptedname, self.tnrs_binomial])
         if 'NA' in nameset: nameset.remove('NA')
         
-        self.nameset = nameset
+        for name in nameset:
+            taxid = genbank.get_taxid(name)
+            taxids[name] = taxid
         
-        logging.info("Created %s", self)
-    
-    def __str__(self):
-        return self.name
+        taxid_set = set(taxids.values())
+        if 'NA' in taxid_set: taxid_set.remove('NA')
+
+        if len(taxid_set)>1:
+            out = json.dumps(taxids, indent=2)
+            log.warning("More than one taxon ID found: \n%s" %taxids)
+            self.taxid = "NA"
+            log.info("TaxonID set to 'NA'")
+        if len(taxid_set)==1:
+            self.taxid = list(taxid_set)[0]
+            log.info("TaxonID set to %s" % self.taxid)
+        if len(taxid_set)==0:
+            log.warning("No taxon ID found")
+            self.taxid = "NA"
+            log.info("TaxonID set to 'NA'")
+                    
 
 
 if __name__ == "__main__":
 
     a = Species("Eucalyptos globulus")
-    
-    print(a)
-    
-    print a.name
-    print a.genus
-    print a.species
-    print a.binomial
-    print a.taxid
-    print a.lineage
-    print a.genbank_binomial
-    print a.tnrs_acceptedname
-    print a.nameset
-    
+            
 
          
