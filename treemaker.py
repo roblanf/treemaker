189355e1baf761564f380c5703d52de24b9b471e
RobertLanfear <rob.lanfear@gmail.com>
2013-05-20 13:48:49 +1000

    include outgroup argument

--- treemaker.py
+++ treemaker.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     # Well behaved unix programs exits with 0 on success...
     sppfile = "/Users/Rob/Dropbox/Current_work/Lasantha/treemaker_input/spplist.txt"
     alnfile = "/Users/Rob/Dropbox/Current_work/Lasantha/treemaker_input/big_plants.cn.phy"
-    match_genera = False
+    outgp = "Ginkgo_biloba"
     
     
-    sys.exit(main.main(sppfile, alnfile, match_genera))
+    sys.exit(main.main(sppfile, alnfile, outgp))
